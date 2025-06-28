cdf2c7e045c06b05831d2188fbd5ed8c217a3dbf
KallDD <kakorpel03@gmail.com>
2025-06-28 23:31:22 +0300

    uibuild

--- part3/puhelinluetteloBackend/index.js
+++ part3/puhelinluetteloBackend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('body', (request) => JSON.stringify(request.body))
@@ -85,7 +86,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
